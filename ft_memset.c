fafc1fe848abec2837ab92649810ad1c5d900667
mac <mac@macs-MacBook-Pro.local>
2023-07-13 19:26:58 +0400

    my first commit

---
+++ ft_memset.c
@@ -0,0 +1,15 @@
+ void * memset(void *b, int c, size_t len)
+ {
+    unsigned char *str;
+
+    if(!b)
+        return (NULL);
+    str = b;
+    while (len != 0)
+    {
+        *str = (unsigned char)c;
+        str++;
+        len--;
+    }
+    return (b);
+ }
